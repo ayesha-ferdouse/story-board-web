86b89683e7450bcb199ab26833859d73c8f4b7aa
ayesha-ferdouse <145672196+ayesha-ferdouse@users.noreply.github.com>
2026-03-25 14:09:21 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <main className="antialiased selection:bg-yellow-200">
+      <Home />
+    </main>
+  );
+}
+
+export default App;
