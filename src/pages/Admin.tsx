7c544b38e8039495d9374e782a977047b9f157da
ayesha-ferdouse <145672196+ayesha-ferdouse@users.noreply.github.com>
2026-03-25 15:26:34 +0200

    Update Admin.tsx

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -2,7 +2,6 @@ import { Studio } from 'sanity'
 import config from '../../sanity.config'
 
 export default function AdminPage() {
-  // This takes your config file and turns it into a full editor
   return (
     <div className="h-screen w-full">
       <Studio config={config} />
