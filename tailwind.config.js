c3d07488bdb99dd3de46261afd255823300c624b
ayesha-ferdouse <145672196+ayesha-ferdouse@users.noreply.github.com>
2026-03-25 14:52:29 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-bg': '#F9FAEB',
+        'brand-text': '#1C242D',
+        'brand-primary': '#D1E231',
+        'brand-secondary': '#EC3031',
+      },
+      fontFamily: {
+        sans: ['"Space Grotesk"', 'sans-serif'],
+        accent: ['"Caveat"', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
