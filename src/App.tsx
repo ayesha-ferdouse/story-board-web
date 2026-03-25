3a5fcd8545fcd36e501572e0897363ffef612768
ayesha-ferdouse <145672196+ayesha-ferdouse@users.noreply.github.com>
2026-03-25 15:34:08 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* The Pear-inspired Home Page */}
         <Route path="/" element={<Home />} />
-        
-        {/* The 9-Years, 11-Countries About Page */}
         <Route path="/about" element={<About />} />
-        
-        {/* The Story Board CMS Editor */}
         <Route path="/admin/*" element={<Admin />} />
       </Routes>
     </BrowserRouter>
