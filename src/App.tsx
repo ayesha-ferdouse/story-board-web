4b572c743e8336ada7ccea2cd567c8aa19d4739b
ayesha-ferdouse <145672196+ayesha-ferdouse@users.noreply.github.com>
2026-03-25 16:05:45 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import Admin from './pages/Admin';
-import Blog from './pages/Blog';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
         <Route path="/admin/*" element={<Admin />} />
       </Routes>
     </BrowserRouter>
