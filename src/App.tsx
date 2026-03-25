233c07b836c1eefef90f7ee6dbb5eaf6009d6c7b
ayesha-ferdouse <145672196+ayesha-ferdouse@users.noreply.github.com>
2026-03-25 15:25:52 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// These must match your file names in src/pages exactly
 import Home from './pages/Home';
 import About from './pages/About';
 import Admin from './pages/Admin';
@@ -17,7 +15,6 @@ function App() {
         <Route path="/about" element={<About />} />
         
         {/* The Story Board CMS Editor */}
-        {/* The asterisk (*) allows Sanity to handle its internal sub-pages */}
         <Route path="/admin/*" element={<Admin />} />
       </Routes>
     </BrowserRouter>
