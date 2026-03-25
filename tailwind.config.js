f3a354f6059013246127fba0365ae65351826b59
ayesha-ferdouse <145672196+ayesha-ferdouse@users.noreply.github.com>
2026-03-25 15:19:24 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        'brand-bg': '#F9FAEB',
-        'brand-text': '#1C242D',
-        'brand-primary': '#D1E231',
-        'brand-secondary': '#EC3031',
+        'brand-bg': '#fff9f5',
+        'brand-dark': '#1C242D',
+        'brand-primary': '#F15425',
+        'brand-secondary': '#7079DD',
+        'brand-body': '#696969',
+        'brand-pink': '#FFD3E2',
+        'brand-teal': '#1B4A48',
       },
       fontFamily: {
         sans: ['"Space Grotesk"', 'sans-serif'],
         accent: ['"Caveat"', 'cursive'],
       },
+      boxShadow: {
+        'neo': '8px 8px 0px 0px rgba(28, 36, 45, 1)',
+        'neo-hover': '12px 12px 0px 0px rgba(28, 36, 45, 1)',
+      }
     },
   },
   plugins: [],
